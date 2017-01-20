7a6e549955830fb4ae648041a7ff37eaec6faaa9
acolletti21 <ashleymcolletti@gmail.com>
2017-01-19 21:32:47 -0600

    updated with restful routing

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,23 +1,39 @@
-class RecipesController < ApplicationController
-  def one_recipe
-    @recipe = Recipe.first
-  end
+# class RecipesController < ApplicationController
+#   def one_recipe
+#     @recipe = Recipe.first
+#   end
 
-  def all_recipes
+#   def all_recipes
+#     @recipes = Recipe.all
+#   end
+
+#   def recipe_form
+
+#   end
+
+#   def recipe_success
+#     @recipe = Recipe.new(
+#                         title: params["title"],
+#                         chef: params["chef"],
+#                         ingredients: params["ingredients"],
+#                         directions: params["directions"]
+#                           )
+#     @recipe.save
+#   end
+# end
+#round1^
+
+
+class RecipesController < ApplicationController
+  
+  def index
     @recipes = Recipe.all
   end
 
-  def recipe_form
-
+  def show
+    @recipe = Recipe.find(params[:id])
   end
 
-  def recipe_success
-    @recipe = Recipe.new(
-                        title: params["title"],
-                        chef: params["chef"],
-                        ingredients: params["ingredients"],
-                        directions: params["directions"]
-                          )
-    @recipe.save
-  end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
-Rails.application.routes.draw do
-  get '/one_recipe_url' => 'recipes#one_recipe'
-  get '/all_recipes_url' => 'recipes#all_recipes'
+# Rails.application.routes.draw do
+#   get '/one_recipe_url' => 'recipes#one_recipe'
+#   get '/all_recipes_url' => 'recipes#all_recipes'
 
-  get '/recipe_form' => 'recipes#recipe_form'
-  post '/recipe_success' => 'recipes#recipe_success'
+#   get '/recipe_form' => 'recipes#recipe_form'
+#   post '/recipe_success' => 'recipes#recipe_success'
+# end
+#round 1^
+
+Rails.application.routes.draw do
+  get '/recipes' => 'recipes#index'
+  get '/recipes/:id' => 'recipes#show'
 end
+
