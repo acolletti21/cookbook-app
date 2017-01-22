12695bdeb88f40649fa080b8b0374d9c84b204eb
acolletti21 <ashleymcolletti@gmail.com>
2017-01-22 10:51:41 -0600

    added the 7 different restful routing options

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 # class RecipesController < ApplicationController
-#   def one_recipe
-#     @recipe = Recipe.first
-#   end
+   # def one_recipe
+   #   @recipe = Recipe.first
+   # end
 
 #   def all_recipes
 #     @recipes = Recipe.all
@@ -30,8 +30,40 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
+  def new
+  end
+
+  def create
+    @recipe = Recipe.new(
+                        title: params[:title],
+                        chef: params[:chef],
+                        ingredients: params[:ingredients],
+                        directions: params[:directions]
+                        )
+    @recipe.save
+  end
+
   def show
     @recipe = Recipe.find(params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find{params[:id]}
+  end
+
+  def update
+    @recipe = Recipe.find{params[:id]}
+    @recipe.title = params[:title]
+    @recipe.chef = params[:chef]
+    @recipe.ingredients =  params[:ingredients]
+    @recipe.directions = params[:directions]
+
+    @recipe.save
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
   end
 
 end
