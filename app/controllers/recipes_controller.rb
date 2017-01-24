f0a0675e04a67298a95c71b690f04a958a02582b
acolletti21 <ashleymcolletti@gmail.com>
2017-01-23 21:24:34 -0600

    model methods, index update, css styling, show page updates

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -27,7 +27,7 @@
 class RecipesController < ApplicationController
   
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.all.order(:id)
   end
 
   def new
@@ -41,6 +41,8 @@ class RecipesController < ApplicationController
                         directions: params[:directions]
                         )
     @recipe.save
+    flash[:success] = "Recipe Created Successfully."
+    redirect_to "/recipes/#{@recipe.id}"
   end
 
   def show
@@ -48,22 +50,27 @@ class RecipesController < ApplicationController
   end
 
   def edit
-    @recipe = Recipe.find{params[:id]}
+    @recipe = Recipe.find(params[:id])
   end
 
   def update
-    @recipe = Recipe.find{params[:id]}
-    @recipe.title = params[:title]
-    @recipe.chef = params[:chef]
-    @recipe.ingredients =  params[:ingredients]
-    @recipe.directions = params[:directions]
-
+    @recipe = Recipe.find(params[:id])
+    @recipe.assign_attributes(
+                        title: params[:title],
+                        chef: params[:chef],
+                        ingredients: params[:ingredients],
+                        directions: params[:directions]
+                        )
     @recipe.save
+    flash[:success] = "Recipe Updated"
+    redirect_to "/recipes/#{@recipe.id}"
   end
 
   def destroy
     @recipe = Recipe.find(params[:id])
     @recipe.destroy
+    flash[:warning] = "Recipe #{@recipe.title} Destroyed"
+    redirect_to "/recipes"
   end
 
 end
